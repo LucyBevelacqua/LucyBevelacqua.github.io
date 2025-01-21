2c151a9a99434312c604a6c8ec6fbe307ada9882
LucyBevelacqua <lucy.bevelacqua@gmail.com>
2025-01-20 21:29:40 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelectorAll("input[type=submit]").forEach((button) => {
+    button.addEventListener("click", (event) => {
+        event.preventDefault();
+        alert(`You clicked ${event.target.value}`);
+    });
+});
