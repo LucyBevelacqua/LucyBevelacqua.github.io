3964c0375b0891571a78db8ede17f3eb459b2b9a
LucyBevelacqua <lucy.bevelacqua@gmail.com>
2025-01-21 09:31:13 -0600

    minor bug fix script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ console.log("JavaScript is connected!");
 document.querySelectorAll("input[type=submit]").forEach((button) => {
     button.addEventListener("click", (event) => {
         event.preventDefault();
-        alert(`You clicked ${event.target.value}`);
+        alert('You clicked ${event.target.value}');
     });
 });
